d128bf09e429fdab0f3c925887851cd664b11a12
Sedanah Mashhour <mashhour.h@northeastern.edu>
2023-12-31 15:56:43 -0500

    updated front page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,8 +16,6 @@ const Navbar = () => {
           <p><a href="#home">Home</a></p>
           <p><a href="#wgpt3">What is GPT3?</a></p>
           <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -33,13 +31,9 @@ const Navbar = () => {
           <div className="gpt3__navbar-menu_container-links">
             <p><a href="#home">Home</a></p>
             <p><a href="#wgpt3">What is GPT3?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
           </div>
           <div className="gpt3__navbar-menu_container-links-sign">
             <p>Sign in</p>
-            <button type="button">Sign up</button>
           </div>
         </div>
         )}
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,17 +4,16 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <h1 className="gradient__text">Unlock Your Digital Ally: AI Guidance in the Cyber Realm</h1>
       <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
       <div className="gpt3__header-content__input">
-        <input type="email" placeholder="Your Email Address" />
-        <button type="button">Get Started</button>
+        <button type="button">Request Access</button>
+        
       </div>
 
       <div className="gpt3__header-content__people">
       
-        <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
 
